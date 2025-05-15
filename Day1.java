b1a15be39818a2e4e47b351801dc62c3e947d71b
PSMahi <panditisaimahesh1999@gmail.com>
2025-05-16 00:39:46 +0530

    Update Day11.java

--- Day1.java
+++ Day1.java
@@ -1,10 +1,10 @@
-package checking;
+package checking1;
 
-public class Day1 {
+public class Day11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("11Hello world");
 
 	}
 
